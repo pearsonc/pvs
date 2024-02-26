b501cc9dde92b4e1560dab20e215cd6a0884bbf7
Chris Pearson <chris@pearsonnet.co.uk>
2024-02-26 13:42:32 +0000

    Updated logging type and changed logging file name

--- logconfig/logconfig.go
+++ logconfig/logconfig.go
@@ -9,8 +9,8 @@ var Log *logrus.Logger
 
 func init() {
 	Log = logrus.New()
-	Log.SetFormatter(&logrus.JSONFormatter{}) // Or TextFormatter if you prefer
-	file, err := os.OpenFile("app.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	Log.SetFormatter(&logrus.TextFormatter{}) // Or TextFormatter if you prefer
+	file, err := os.OpenFile("pvs.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err == nil {
 		Log.SetOutput(file)
 	} else {
